f6b4e5ba7401da601a19d27f8bd3b0e1027b32fd
namhihi237 <yentth237@gmail.com>
2022-06-07 23:46:28 +0700

    fix: handle error common and fix user controller error

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,8 +2,11 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const userRouter = express.Router();
 
+// @ts-ignore
 userRouter.get('/users', userController.getAll);
+// @ts-ignore
 userRouter.get('/users/:id', userController.deleteUser);
+// @ts-ignore
 userRouter.put('/users/:id', userController.update);
 userRouter.get('/users/:id', userController.user);
 
